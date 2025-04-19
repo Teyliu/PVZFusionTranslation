400fe3d3cb5165dc9cad7b09cf56affe5ea681da
Teyliu <teyliu888@gmail.com>
2025-04-19 11:44:12 -0500

    INIT

---
+++ PvZ-Fusion-Blooms-master/MelonLoader/Patches/BaseTextObjects/InGameText_Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTMPro;
+using PvZ_Fusion_Translator.AssetStore;
+
+namespace PvZ_Fusion_Translator.Patches.BaseTextObjects
+{
+	[HarmonyPatch(typeof(InGameText))]
+	public static class InGameText_Patch
+	{
+		[HarmonyPatch(nameof(InGameText.ShowText), new Type[] { typeof(string), typeof(float), typeof(bool) })]
+		[HarmonyPrefix]
+		private static void ShowText(InGameText __instance, ref string text) => text = StringStore.TranslateText(text, true);
+
+		[HarmonyPatch(nameof(InGameText.ShowText), new Type[] { typeof(string), typeof(float), typeof(bool) })]
+		[HarmonyPostfix]
+		private static void ShowText(InGameText __instance)
+		{
+
+			#if MULTI_LANGUAGE
+			TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+			#else
+			TMP_FontAsset fontAsset = FontStore.LoadTMPFont();
+			#endif
+			
+			foreach (TextMeshProUGUI txt in __instance.textMeshes)
+			{
+				txt.text = StringStore.TranslateText(txt.text, true);
+                txt.font = fontAsset;
+			}
+		}
+	}
+}
---
+++ PvZ-Fusion-Blooms-master/MelonLoader/Patches/GameObjects/NoticePauseMenu_Patch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTMPro;
+using PvZ_Fusion_Translator.AssetStore;
+using UnityEngine;
+
+namespace PvZ_Fusion_Translator.Patches.GameObjects
+{
+    [HarmonyPatch(typeof(NoticeMenu))]
+	public class NoticePauseMenu_Patch
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch(nameof(NoticeMenu.Start))]
+		private static void Start(NoticeMenu __instance)
+		{
+			TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+			Transform warningTransform = __instance.transform.GetChild(0);
+			Transform warningTextTransform = warningTransform.FindChild("文字");
+			Transform warningTextShadowTransform = warningTransform.FindChild("文字2");
+			Transform[] array = [warningTextTransform, warningTextShadowTransform];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] != null)
+				{
+					array[i].GetComponent<TextMeshProUGUI>().text = StringStore.TranslateText(array[i].GetComponent<TextMeshProUGUI>().text);
+					array[i].GetComponent<TextMeshProUGUI>().font = fontAsset;
+				}
+			}
+		}
+	}
+}
