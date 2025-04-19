85dfdbb28b4900a49ff0681b3ed7f5a39f83f193
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-04-19 13:36:51 -0400

    my code

--- MelonLoader/Patches/GameObjects/ButtonObjects/AbyssButton_Patch.cs
+++ MelonLoader/Patches/GameObjects/ButtonObjects/AbyssButton_Patch.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 
 namespace PvZ_Fusion_Translator.Patches.GameObjects.ButtonObjects
 {
-    [HarmonyPatch(typeof(AbyssButton))]
-    public static class AbyssButton_Patch
-    {
-        [HarmonyPatch(nameof(AbyssButton.Start))]
-        [HarmonyPostfix]
-        private static void Start(AbyssButton __instance)
-        {
-            StringStore.TranslateTextTransform(__instance.transform, true);
-            Transform parentTransform = __instance.transform.parent.transform;
-			if (__instance.name == "Goback")
-			{
-				Transform backgroundTransform = parentTransform.Find("Background");
-				backgroundTransform.Find("Name").GetComponent<TMP_Text>().autoSizeTextContainer = true;
-				backgroundTransform.Find("Shadow").GetComponent<TMP_Text>().autoSizeTextContainer = true;
-			}
-        }
-    }
+   // [HarmonyPatch(typeof(AbyssBuffButton))]
+   // public static class AbyssButton_Patch
+   // {
+   //     [HarmonyPatch(nameof(AbyssBuffButton.Start))]
+   //     [HarmonyPostfix]
+   //     private static void Start(AbyssBuffButton __instance)
+   //     {
+   //         StringStore.TranslateTextTransform(__instance.transform, true);
+   //         Transform parentTransform = __instance.transform.parent.transform;
+			//if (__instance.name == "Goback")
+			//{
+			//	Transform backgroundTransform = parentTransform.Find("Background");
+			//	backgroundTransform.Find("Name").GetComponent<TMP_Text>().autoSizeTextContainer = true;
+			//	backgroundTransform.Find("Shadow").GetComponent<TMP_Text>().autoSizeTextContainer = true;
+			//}
+   //     }
+   // }
 }
--- MelonLoader/Patches/GameObjects/TravelRefresh_Patch.cs
+++ MelonLoader/Patches/GameObjects/TravelRefresh_Patch.cs
@@ -6,48 +6,93 @@ using UnityEngine;
 
 namespace PvZ_Fusion_Translator.Patches.GameObjects
 {
-    [HarmonyPatch(typeof(TravelRefresh))]
+	[HarmonyPatch(typeof(TravelRefresh))]
 	public static class TravelRefresh_Patch
 	{
-		[HarmonyPatch(nameof(TravelRefresh.Update))]
+		[HarmonyPatch(nameof(TravelRefresh.Start))]
 		[HarmonyPostfix]
-		private static void Update(TravelRefresh __instance)
+		private static void Start(TravelRefresh __instance)
 		{
-			TextMeshProUGUI pointsText = __instance.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-			pointsText = StringStore.TranslateText(pointsText);
-			pointsText.autoSizeTextContainer = true;
+            __instance.text = StringStore.TranslateText(__instance.text);
+            __instance.text_shadow = StringStore.TranslateText(__instance.text_shadow);
+
+            //TextMeshProUGUI pointsText = __instance.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            //pointsText = StringStore.TranslateText(pointsText);
+            //pointsText.autoSizeTextContainer = true;
 
 
-			Transform TravelMenuTransform = __instance.transform.parent.transform;
+            //Transform TravelMenuTransform = __instance.transform.parent.transform;
 
-			Transform refreshTransform = TravelMenuTransform.Find("Refresh2").transform.Find("bank");
-			if (refreshTransform != null)
-			{
-				Transform text_1Transform = refreshTransform.Find("text_1");
-				if (text_1Transform != null)
-				{
-					TextMeshProUGUI text_1 = text_1Transform.GetComponent<TextMeshProUGUI>();
-					
-					if (text_1)
-					{
-						text_1 = StringStore.TranslateText(text_1);
-						text_1.autoSizeTextContainer = true;
-					}
-				}
+            //Transform refreshTransform = TravelMenuTransform.Find("Refresh2").transform.Find("bank");
+            //if (refreshTransform != null)
+            //{
+            //	Transform text_1Transform = refreshTransform.Find("text_1");
+            //	if (text_1Transform != null)
+            //	{
+            //		TextMeshProUGUI text_1 = text_1Transform.GetComponent<TextMeshProUGUI>();
 
-				Transform text_2Transform = refreshTransform.Find("text_2");
-				if (text_2Transform != null)
-				{
-					TextMeshProUGUI text_2 = text_2Transform.GetComponent<TextMeshProUGUI>();
+            //		if (text_1)
+            //		{
+            //			text_1 = StringStore.TranslateText(text_1);
+            //			text_1.autoSizeTextContainer = true;
+            //		}
+            //	}
 
-					if (text_2)
-					{
-                        text_2 = StringStore.TranslateText(text_2);
-                        text_2.autoSizeTextContainer = true;
-					}
-				}
-			}
+            //	Transform text_2Transform = refreshTransform.Find("text_2");
+            //	if (text_2Transform != null)
+            //	{
+            //		TextMeshProUGUI text_2 = text_2Transform.GetComponent<TextMeshProUGUI>();
 
-		}
-	}
+            //		if (text_2)
+            //		{
+            //			text_2 = StringStore.TranslateText(text_2);
+            //			text_2.autoSizeTextContainer = true;
+            //		}
+            //	}
+            //}
+        }
+
+        [HarmonyPatch(nameof(TravelRefresh.OnMouseUpAsButton))]
+        [HarmonyPostfix]
+        private static void OnMouseUpAsButton(TravelRefresh __instance)
+        {
+            __instance.text = StringStore.TranslateText(__instance.text);
+            __instance.text_shadow = StringStore.TranslateText(__instance.text_shadow);
+
+            //TextMeshProUGUI pointsText = __instance.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            //pointsText = StringStore.TranslateText(pointsText);
+            //pointsText.autoSizeTextContainer = true;
+
+
+            //Transform TravelMenuTransform = __instance.transform.parent.transform;
+
+            //Transform refreshTransform = TravelMenuTransform.Find("Refresh2").transform.Find("bank");
+            //if (refreshTransform != null)
+            //{
+            //	Transform text_1Transform = refreshTransform.Find("text_1");
+            //	if (text_1Transform != null)
+            //	{
+            //		TextMeshProUGUI text_1 = text_1Transform.GetComponent<TextMeshProUGUI>();
+
+            //		if (text_1)
+            //		{
+            //			text_1 = StringStore.TranslateText(text_1);
+            //			text_1.autoSizeTextContainer = true;
+            //		}
+            //	}
+
+            //	Transform text_2Transform = refreshTransform.Find("text_2");
+            //	if (text_2Transform != null)
+            //	{
+            //		TextMeshProUGUI text_2 = text_2Transform.GetComponent<TextMeshProUGUI>();
+
+            //		if (text_2)
+            //		{
+            //			text_2 = StringStore.TranslateText(text_2);
+            //			text_2.autoSizeTextContainer = true;
+            //		}
+            //	}
+            //}
+        }
+    }
 }
