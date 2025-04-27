0eb636f3763df46ba3133dba885e331cab30aa27
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-04-27 18:39:14 -0400

    izombie button fix

---
+++ MelonLoader/Patches/GameObjects/ButtonObjects/CustomLevelEnter_Patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTMPro;
+using MelonLoader;
+using PvZ_Fusion_Translator.AssetStore;
+using UnityEngine;
+
+namespace PvZ_Fusion_Translator.Patches.GameObjects.ButtonObjects
+{
+	[HarmonyPatch(typeof(CustomLevelEnter))]
+	public static class CustomLevelEnter_Patch
+    {
+		[HarmonyPatch(nameof(CustomLevelEnter.Start))]
+		[HarmonyPostfix]
+		private static void Show(CustomLevelEnter __instance)
+		{
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            foreach(TextMeshProUGUI txt in __instance.GetComponentsInChildren<TextMeshProUGUI>())
+			{
+				txt.text = StringStore.TranslateText(txt.text);
+				txt.font = fontAsset;
+            }
+		}
+	}
+}
