4aa4dfb4142606d95e9c922f8ca1dc3e78bbd599
TuanAnh2901 <tuananh4c.ltk@gmail.com>
2025-06-06 10:00:26 +0700

    fixed a thing in DrawCard

--- MelonLoader/Patches/Managers/DrawCardManager_Patch.cs
+++ MelonLoader/Patches/Managers/DrawCardManager_Patch.cs
@@ -17,7 +17,7 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
         private static void Postfix_UpdateText(DrawCardManager __instance)
         {
             Il2CppSystem.Collections.Generic.List<TextMeshProUGUI> texts = __instance.texts;
-
+            __instance.upName = StringStore.TranslateText(__instance.upName);
             if (texts != null && texts.Count > 0)
             {
                 foreach (TextMeshProUGUI textComponent in texts)
@@ -32,5 +32,31 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
                 }
             }
         }
+
+
+
+        [HarmonyPatch(nameof(DrawCardManager.Update))]
+        [HarmonyPostfix]
+        private static void Postfix_Update(DrawCardManager __instance)
+        {
+            Il2CppSystem.Collections.Generic.List<TextMeshProUGUI> texts = __instance.texts;
+            __instance.upName = StringStore.TranslateText(__instance.upName);
+
+            if (texts != null && texts.Count > 0)
+            {
+                foreach (TextMeshProUGUI textComponent in texts)
+                {
+                    if (textComponent != null)
+                    {
+                        string translatedText = StringStore.TranslateText(textComponent.text);
+                        TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+                        textComponent.text = translatedText;
+                        textComponent.font = fontAsset;
+                    }
+                }
+            }
+
+        }
     }
 }
+
