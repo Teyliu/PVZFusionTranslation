685db5a19a4d2773eb43a868f61285ac6570e866
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-07-14 15:25:21 -0400

    just updating my branch

--- MelonLoader/Patches/GameObjects/ButtonObjects/UIButton_Patch.cs
+++ MelonLoader/Patches/GameObjects/ButtonObjects/UIButton_Patch.cs
@@ -3,7 +3,6 @@ using Il2Cpp;
 using Il2CppTMPro;
 using PvZ_Fusion_Translator.AssetStore;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 namespace PvZ_Fusion_Translator.Patches.GameObjects
@@ -15,27 +14,59 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
         [HarmonyPostfix]
         private static void Postfix_Start(UIButton __instance)
         {
-            Text[] textComponents = __instance.GetComponentsInChildren<Text>(true);
-            
-            foreach (Text textComponent in textComponents)
+            try
             {
-                if (textComponent != null && !string.IsNullOrEmpty(textComponent.text))
+                // Convert and translate legacy Text components
+                Text[] textComponents = __instance.GetComponentsInChildren<Text>(true);
+                foreach (Text textComponent in textComponents)
                 {
-                    string translatedText = StringStore.TranslateText(textComponent.text);
-                    if (!string.IsNullOrEmpty(translatedText) && translatedText != textComponent.text)
+                    if (textComponent != null && !string.IsNullOrEmpty(textComponent.text))
                     {
-                        TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
-
-                        TextMeshProUGUI newTMP = StringStore.ConvertToTextMeshPro(textComponent, fontAsset, true);
-
-                        if (newTMP != null)
+                        string translatedText = StringStore.TranslateText(textComponent.text);
+                        if (!string.IsNullOrEmpty(translatedText) && translatedText != textComponent.text)
                         {
-                            newTMP.text = translatedText;
-                            //Debug.Log($"Converted and translated text: {textComponent.text} -> {translatedText}");
+                            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+                            TextMeshProUGUI newTMP = StringStore.ConvertToTextMeshPro(textComponent, fontAsset, true);
+
+                            if (newTMP != null)
+                            {
+                                newTMP.text = translatedText;
+                            }
                         }
                     }
                 }
+
+                // Also handle TMP components directly under the button
+                if (__instance.transform.childCount >= 2)
+                {
+                    TextMeshProUGUI text = __instance.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    TextMeshProUGUI textShadow = __instance.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+                    StringStore.TranslateTextUI(text, false);
+                    StringStore.TranslateTextUI(textShadow, false);
+                    text.autoSizeTextContainer = textShadow.autoSizeTextContainer = true;
+                }
             }
+            catch (Exception) { }
+        }
+
+        [HarmonyPatch(nameof(UIButton.OnMouseUpAsButton))]
+        [HarmonyPostfix]
+        public static void Postfix_OnMouseUpAsButton(UIButton __instance)
+        {
+            try
+            {
+                if (__instance.transform.childCount >= 2)
+                {
+                    TextMeshProUGUI text = __instance.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    TextMeshProUGUI textShadow = __instance.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+                    StringStore.TranslateTextUI(text, false);
+                    StringStore.TranslateTextUI(textShadow, false);
+                    text.autoSizeTextContainer = textShadow.autoSizeTextContainer = true;
+                }
+            }
+            catch (Exception) { }
         }
     }
 }
