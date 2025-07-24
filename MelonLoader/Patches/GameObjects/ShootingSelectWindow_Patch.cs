d49fae11bbc68a00b690a9081e9943e164927c2f
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-07-24 12:20:19 -0400

    Update ShootingSelectWindow_Patch.cs

--- MelonLoader/Patches/GameObjects/ShootingSelectWindow_Patch.cs
+++ MelonLoader/Patches/GameObjects/ShootingSelectWindow_Patch.cs
@@ -10,7 +10,7 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
     [HarmonyPatch(typeof(ShootingSelectWindow))]
     public static class ShootingSelectWindow_Patch
     {
-        [HarmonyPatch(nameof(ShootingSelectWindow.SetUpgradePlantTypes))]
+        [HarmonyPatch(nameof(ShootingSelectWindow.InitPlantType))]
         [HarmonyPostfix]
         private static void InitCard(ShootingSelectWindow __instance)
         {
@@ -25,6 +25,42 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
         [HarmonyPatch(nameof(ShootingSelectWindow.OnAnimOver))]
         [HarmonyPostfix]
         private static void OnAnimOver(ShootingSelectWindow __instance)
+        {
+            Transform introduceTransform = __instance.transform.FindChild("Images").FindChild("Introduce");
+            TextMeshProUGUI introduceText = introduceTransform.GetComponent<TextMeshProUGUI>();
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            introduceText.text = StringStore.TranslateText(introduceText.text);
+            introduceText.font = fontAsset;
+        }
+
+        [HarmonyPatch(nameof(ShootingSelectWindow.SetUpgradePlantTypes))]
+        [HarmonyPostfix]
+        private static void SetUpgradePlantTypes(ShootingSelectWindow __instance)
+        {
+            Transform introduceTransform = __instance.transform.FindChild("Images").FindChild("Introduce");
+            TextMeshProUGUI introduceText = introduceTransform.GetComponent<TextMeshProUGUI>();
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            introduceText.text = StringStore.TranslateText(introduceText.text);
+            introduceText.font = fontAsset;
+        }
+
+        [HarmonyPatch(nameof(ShootingSelectWindow.GetMaxIndexInRoutes))]
+        [HarmonyPostfix]
+        private static void GetMaxIndexInRoutes(ShootingSelectWindow __instance)
+        {
+            Transform introduceTransform = __instance.transform.FindChild("Images").FindChild("Introduce");
+            TextMeshProUGUI introduceText = introduceTransform.GetComponent<TextMeshProUGUI>();
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            introduceText.text = StringStore.TranslateText(introduceText.text);
+            introduceText.font = fontAsset;
+        }
+
+        [HarmonyPatch(nameof(ShootingSelectWindow.Start))]
+        [HarmonyPostfix]
+        private static void Start(ShootingSelectWindow __instance)
         {
             Transform introduceTransform = __instance.transform.FindChild("Images").FindChild("Introduce");
             TextMeshProUGUI introduceText = introduceTransform.GetComponent<TextMeshProUGUI>();
