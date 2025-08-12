c5cfa2cbb713d71d2996ddec1484ce7f3ff7855e
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-08-12 15:54:30 -0400

    garden plant and reroll text patches

--- MelonLoader/Patches/GameObjects/GardenPlant_Patch.cs
+++ MelonLoader/Patches/GameObjects/GardenPlant_Patch.cs
@@ -1,24 +1,28 @@
 ﻿using HarmonyLib;
 using Il2Cpp;
 using PvZ_Fusion_Translator.AssetStore;
+using System.Text.RegularExpressions;
 
 namespace PvZ_Fusion_Translator.Patches.GameObjects
 {
     [HarmonyPatch(typeof(GardenPlant))]
-	public static class GardenPlant_Patch
-	{
-		[HarmonyPatch(nameof(GardenPlant.Update))]
-		[HarmonyPostfix]
-		private static void Update(GardenPlant __instance)
-		{
-			__instance.infoText = StringStore.TranslateText(__instance.infoText);
-		}
+    public static class GardenPlant_Patch
+    {
+        [HarmonyPatch(nameof(GardenPlant.Start))]
+        [HarmonyPostfix]
+        private static void Start(GardenPlant __instance)
+        {
+            __instance.infoText.font = FontStore.LoadTMPFont(Utils.Language.ToString());
+            __instance.infoText.text = StringStore.TranslateText(__instance.infoText.text);
+        }
 
-		[HarmonyPatch(nameof(GardenPlant.UpdateInfo))]
-		[HarmonyPostfix]
-		private static void UpdateInfo(GardenPlant __instance)
-		{
-			__instance.infoText = StringStore.TranslateText(__instance.infoText);
-		}
-	}
+        [HarmonyPatch(nameof(GardenPlant.UpdateInfo))]
+        [HarmonyPostfix]
+        private static void UpdateInfo(GardenPlant __instance)
+        {
+            
+            __instance.infoText.font = FontStore.LoadTMPFont(Utils.Language.ToString());
+            __instance.infoText.text = StringStore.TranslateText(__instance.infoText.text);
+        }
+    }
 }
