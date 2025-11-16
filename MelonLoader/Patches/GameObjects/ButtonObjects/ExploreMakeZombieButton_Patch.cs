5ed6b1a32550fec561de6766f485354b7d4f5b04
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-11-16 01:07:27 -0500

    3.1 code draft complete!!
     fusion showcase editor code hopefully done

---
+++ MelonLoader/Patches/GameObjects/ButtonObjects/ExploreMakeZombieButton_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTMPro;
+using PvZ_Fusion_Translator.AssetStore;
+using UnityEngine;
+
+namespace PvZ_Fusion_Translator.Patches.GameObjects.ButtonObjects
+{
+	[HarmonyPatch(typeof(ExploreMakeZombieButton))]
+	public class ExploreMakeZombieButton_Patch
+    {
+		[HarmonyPostfix]
+		[HarmonyPatch(nameof(ExploreMakeZombieButton.Start))]
+		public static void Start(ExploreMakeZombieButton __instance)
+		{
+			__instance.sceneText.text = Utils.GetZombieNameFromAlmanac(__instance.theZombieType);
+			__instance.sceneText.font = FontStore.LoadTMPFont(Utils.Language.ToString());
+        }
+	}
+}
--- MelonLoader/Patches/GameObjects/ExploreMakeMenu_Patch.cs
+++ MelonLoader/Patches/GameObjects/ExploreMakeMenu_Patch.cs
@@ -2,6 +2,7 @@
 using Il2Cpp;
 using Il2CppTMPro;
 using PvZ_Fusion_Translator.AssetStore;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace PvZ_Fusion_Translator.Patches.GameObjects
@@ -18,6 +19,53 @@ namespace PvZ_Fusion_Translator.Patches.GameObjects
             foreach (TextMeshProUGUI txt in __instance.GetComponentsInChildren<TextMeshProUGUI>())
             {
                 txt.text = StringStore.TranslateText(txt.text);
+                txt.font = fontAsset;
+            }
+        }
+
+        [HarmonyPatch(nameof(ExploreMakeMenu.InitMenu))]
+        [HarmonyPostfix]
+        private static void InitMenu(ExploreMakeMenu __instance)
+        {
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            foreach (TextMeshProUGUI txt in __instance.GetComponentsInChildren<TextMeshProUGUI>())
+            {
+                if(txt.text.Contains("关卡制作中："))
+                {
+                    string plantName = Utils.GetPlantNameFromAlmanac(txt.text.Split("：")[1]);
+                    txt.text = String.Format(StringStore.translationStringRegex["关卡制作中：([^\\s]+)"], plantName);
+                }
+                else
+                {
+                    txt.text = StringStore.TranslateText(txt.text);
+                }
+            }
+        }
+
+        [HarmonyPatch(nameof(ExploreMakeMenu.ViewScene))]
+        [HarmonyPostfix]
+        private static void ViewScene(ExploreMakeMenu __instance)
+        {
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            foreach (TextMeshProUGUI txt in __instance.GetComponentsInChildren<TextMeshProUGUI>())
+            {
+                txt.text = StringStore.TranslateText(txt.text);
+                txt.font = fontAsset;
+            }
+        }
+
+        [HarmonyPatch(nameof(ExploreMakeMenu.InitZombieButtons))]
+        [HarmonyPostfix]
+        private static void InitZombieButtons(ExploreMakeMenu __instance)
+        {
+            TMP_FontAsset fontAsset = FontStore.LoadTMPFont(Utils.Language.ToString());
+
+            foreach (TextMeshProUGUI txt in __instance.GetComponentsInChildren<TextMeshProUGUI>())
+            {
+                txt.text = StringStore.TranslateText(txt.text);
+                txt.font = fontAsset;
             }
         }
     }
