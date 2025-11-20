ce822a5d06910b2648951a38c352ec1a6490d596
SillyStar-Github <90102598+SillyStar-Github@users.noreply.github.com>
2025-11-19 19:01:12 -0500

    button fixes (back to index, almanac skin button logging an error)

---
+++ MelonLoader/Patches/GameObjects/ButtonObjects/BackToIndex_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppTMPro;
+using PvZ_Fusion_Translator.AssetStore;
+using UnityEngine;
+
+namespace PvZ_Fusion_Translator.Patches.GameObjects.ButtonObjects
+{
+	[HarmonyPatch(typeof(BackToIndex))]
+	public class BackToIndex_Patch
+    {
+		[HarmonyPostfix]
+		[HarmonyPatch(nameof(BackToIndex.Awake))]
+		public static void Awake(BackToIndex __instance)
+		{
+            StringStore.TranslateTextTransform(__instance.transform.GetChild(0));
+        }
+	}
+}
